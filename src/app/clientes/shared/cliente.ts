2dab0bc3bc11d910cf9464d16b0dad733e65731a
augusto <Augusto@sunter.com.br>
2021-07-01 22:57:04 -0300

    angular com reactive forms

---
+++ src/app/clientes/shared/cliente.ts
@@ -0,0 +1,9 @@
+export class Cliente {
+    nome: string = '';
+    tipo: number = 1;
+    genero: number = 1;
+    dataNascimento: Date = new Date();
+    observacao: string = '';
+    inativo: boolean = false;
+
+}
